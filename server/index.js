09983ffb674ac742c58d5f0e839c389010dd047e
flushthemoney <skaviraj562@gmail.com>
2025-02-06 01:11:34 +0530

    remove comments and unnecessary code
    Signed-off-by: flushthemoney <skaviraj562@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cron = require("cron");
-const UptimeService = require("./services/uptimeService"); // Import UptimeService
 const logger = require("./utils/logger");
 const uptimeRoutes = require("./routes/uptimeRoutes");
 const cors = require("cors");
@@ -9,20 +7,13 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8001;
 
 app.use(express.json());
 app.use(cors());
 
 // Routes
 app.use("/api", uptimeRoutes);
-
-// const job = new cron.CronJob(process.env.CHECK_INTERVAL, async () => {
-//   const status = await UptimeService.checkUptime();
-//   logger.info(`Scheduled check: Instagram is ${status}`);
-// });
-
-//job.start();
 
 app.listen(PORT, () => {
   logger.info(`Server is running on port ${PORT}`);
